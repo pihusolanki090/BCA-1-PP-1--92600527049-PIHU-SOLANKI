2d8d5ff1f025896c10023c07fcb7c69c24063bb1
pihusolanki090 <pihusolanki090@gmail.com>
2026-08-06 14:52:05 +0530

    Add files via upload

---
+++ U2-9.C
@@ -0,0 +1,24 @@
+//wap to interchange two number without using 3rd varible
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+	int x,y;
+	clrscr();
+	printf("\n Enter value of x :");
+	scanf("%d", &x);
+
+	printf("\n Enter value of y :");
+	scanf("%d", &y);
+
+	x = x + y;
+	y = x - y;
+	x = x - y;
+
+	printf("\n after interchange : x =%d,y =%d " ,x,y);
+
+	getch();
+}
+
