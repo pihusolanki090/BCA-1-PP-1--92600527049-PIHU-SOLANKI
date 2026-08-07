7bb2efa803c3215c549505adb6be9c0a10ffb8eb
pihusolanki090 <pihusolanki090@gmail.com>
2026-08-07 11:31:20 +0530

    Add files via upload

---
+++ U3-2.C
@@ -0,0 +1,23 @@
+//wap that input 2 values from user and find out minimum
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b;
+	clrscr();
+
+	printf("\n Enter value of a,b :");
+	scanf("%d%d",&a,&b);
+
+	if(a == b);
+	{
+		printf("\n both are equal");
+
+	}
+	else
+	{
+	   printf
+	}
+	getch();
+}
---
+++ U3-3.C
@@ -0,0 +1,24 @@
+//wap that input age from user if age>=18 then print person is eligible for vote else person is not eligible for vote
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a;
+	clrscr();
+
+	printf("\n Enter  your age :");
+	scanf("%d",&a);
+
+	if (a >= 18)
+	{
+		printf("\n You are eligible for voting");
+
+	}
+	else
+	{
+		printf("\n You are eligible for voting");
+
+	}
+	getch();
+}
