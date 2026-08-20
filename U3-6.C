c3d71f6fa2313a5506fedd65fb1606ebdfc67196
pihusolanki090 <pihusolanki090@gmail.com>
2026-08-20 15:45:16 +0530

    Add files via upload

---
+++ U3-6.C
@@ -0,0 +1,46 @@
+//unit-3 (6) calculate gross salary.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  float s,hr,ta,da,pf,gs;
+  clrscr();
+
+  printf("\n Enter basic salary :");
+  scanf("%f",&s);
+
+  if(s >= 5000)
+  {
+	hr= (s *0.05);
+	ta=(s * 0.06);
+	da=(s * 0.04);
+	pf=(s * 0.05);
+
+
+  }
+  else
+  {
+	hr=(s * 0.04);
+	ta=(s * 0.05);
+	da=(s * 0.03);
+	pf=(s * 0.04);
+
+
+
+  }
+
+  gs= s + hr + ta + da -pf;
+  printf("\n gross salary is :%.2f",gs);
+  printf("\n Hr Rs =%.2f",hr);
+  printf("\n Ta Rs =%.2f",ta);
+  printf("\n Da Rs =%.2f",da);
+  printf("\n Pf Rs =%.2f",pf);
+
+
+
+
+  getch();
+
+}
