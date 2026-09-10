eb84574aae15b6301ae84498a1e0c51e611d982a
pihusolanki090 <pihusolanki090@gmail.com>
2026-09-10 15:56:33 +0530

    Add files via upload

---
+++ ESCHAR.C
@@ -0,0 +1,17 @@
+// global variable demo
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+	int i;
+
+	clrscr();
+	for(i=1;i<=20000;i++)
+	{
+		printf("\a thank you");
+		delay("1000");
+
+	}
+	getch();
+}
